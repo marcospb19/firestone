c7618b821425f6673b6b690f0137357a55683150
João Marcos P. Bezerra <marcospb19@hotmail.com>
2024-06-06 03:02:47 -0300

    add jetpack

--- objects/player.gd
+++ objects/player.gd
@@ -2,7 +2,8 @@ extends CharacterBody3D
 
 @export_subgroup("Properties")
 @export var movement_speed = 5
-@export var jump_strength = 8
+@export var jump_strength = 4
+@export var jet_strength = 23
 
 @export_subgroup("Weapons")
 @export var weapons: Array[Weapon] = []
@@ -13,7 +14,7 @@ var weapon_index := 0
 var mouse_captured := true
 var mouse_input: Vector2
 
-var mouse_sensitivity = 700
+var mouse_sensitivity := 500
 var gamepad_sensitivity := 0.075
 
 var movement_velocity: Vector3
@@ -24,8 +25,7 @@ var gravity := 0.0
 
 var previously_floored := false
 
-var jump_single := true
-var jump_double := true
+var jump := true
 
 var container_offset = Vector3(1.2, -1.1, -2.75)
 
@@ -42,7 +42,6 @@ signal health_updated
 
 @export var crosshair:TextureRect
 
-# Functions
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	
@@ -52,7 +51,9 @@ func _ready():
 func _physics_process(delta):
 	# Handle functions
 	handle_controls(delta)
-	handle_gravity(delta)
+	
+	# apply gravity
+	gravity += 20 * delta
 	
 	# Movement
 	var applied_velocity: Vector3
@@ -63,7 +64,7 @@ func _physics_process(delta):
 	applied_velocity.y = -gravity
 	
 	velocity = applied_velocity
-	move_and_slide()
+	self.move_and_slide()
 	
 	# Rotation
 	camera.rotation.z = -mouse_input.x * 25 * delta
@@ -76,24 +77,23 @@ func _physics_process(delta):
 	# Movement sound
 	sound_footsteps.stream_paused = true
 	
-	if is_on_floor():
+	if self.is_on_floor():
 		if abs(velocity.x) > 1 or abs(velocity.z) > 1:
 			sound_footsteps.stream_paused = false
 	
-	# Landing after jump or falling
+	 #Landing after jump or falling
 	camera.position.y = lerp(camera.position.y, 0.0, delta * 5)
 	
-	if is_on_floor() and gravity > 1 and !previously_floored: # Landed
+	if self.is_on_floor() and gravity > 1 and !previously_floored: # Landed
 		Audio.play("sounds/land.ogg")
 		camera.position.y = -0.1
 	
-	previously_floored = is_on_floor()
+	previously_floored = self.is_on_floor()
 	
 	# Falling/respawning
 	if position.y < -10:
-		get_tree().reload_current_scene()
+		self.get_tree().reload_current_scene()
 
-# Mouse movement
 func _input(event):
 	if event is InputEventMouseMotion and mouse_captured:
 		mouse_input = event.relative / mouse_sensitivity
@@ -101,7 +101,7 @@ func _input(event):
 		rotation_target.y -= event.relative.x / mouse_sensitivity
 		rotation_target.x -= event.relative.y / mouse_sensitivity
 
-func handle_controls(_delta):
+func handle_controls(delta: float):
 	# Mouse capture
 	if Input.is_action_just_pressed("mouse_capture"):
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -124,40 +124,27 @@ func handle_controls(_delta):
 	rotation_target -= Vector3(-rotation_input.y, -rotation_input.x, 0).limit_length(1.0) * gamepad_sensitivity
 	rotation_target.x = clamp(rotation_target.x, deg_to_rad(-90), deg_to_rad(90))
 	
-	# Shooting
 	action_shoot()
+	action_jet(delta)
 	
 	# Jumping
 	if Input.is_action_just_pressed("jump"):
-		if jump_single or jump_double:
+		if jump:
 			Audio.play(["sounds/jump_a.ogg", "sounds/jump_b.ogg", "sounds/jump_c.ogg"])
-			
-		if jump_double:
 			gravity = -jump_strength
-			jump_double = false
-			
-		if jump_single:
-			action_jump()
+			jump = false
+
+	if gravity > 0 and self.is_on_floor():
+		jump = true
+		gravity = 0
 		
 	# Weapon switching
 	action_weapon_toggle()
 
-# Handle gravity
-func handle_gravity(delta):
-	gravity += 20 * delta
-	
-	if gravity > 0 and is_on_floor():
-		jump_single = true
-		gravity = 0
+func action_jet(delta: float):
+	if Input.is_action_pressed("jetpack"):
+		gravity -= jet_strength * delta
 
-# Jumping
-func action_jump():
-	gravity = -jump_strength
-	
-	jump_single = false;
-	jump_double = true;
-
-# Shooting
 func action_shoot():
 	if Input.is_action_pressed("shoot") and blaster_cooldown.is_stopped():
 		Audio.play(weapon.sound_shoot)
@@ -197,7 +184,7 @@ func action_shoot():
 			
 			impact_instance.play("shot")
 			
-			get_tree().root.add_child(impact_instance)
+			self.get_tree().root.add_child(impact_instance)
 			
 			impact_instance.position = raycast.get_collision_point() + (raycast.get_collision_normal() / 10)
 			impact_instance.look_at(camera.global_transform.origin, Vector3.UP, true) 
