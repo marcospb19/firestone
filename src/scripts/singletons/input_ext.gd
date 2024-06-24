7deaee7bdc5b66aa7f13a75184d18e883e0b6e9c
João Marcos P. Bezerra <marcospb19@hotmail.com>
2024-06-24 00:04:13 -0300

    lower FPS when window not focused

--- src/scripts/singletons/input_ext.gd
+++ src/scripts/singletons/input_ext.gd
@@ -12,6 +12,11 @@ func _process(_delta):
 	if Input.is_action_just_pressed("mouse_capture_exit"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	
+	if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		Engine.max_fps = 1000
+	else:
+		Engine.max_fps = 45
+	
 	# Reset game
 	if Input.is_action_just_pressed("reset"):
 		self.get_tree().reload_current_scene()
