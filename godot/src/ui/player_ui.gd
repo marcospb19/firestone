464c6e3428cb5c55e87e21ed6725c9f2eb84334a
João Marcos <marcospb19@hotmail.com>
2025-05-20 00:08:27 -0300

    some renaming

--- godot/src/ui/player_ui.gd
+++ godot/src/ui/player_ui.gd
@@ -1,6 +1,7 @@
 extends Control
 
-@onready var hotbar_selection: TextureRect = $FixedHUD/HotbarSelectionSquare
+@onready var hotbar_selection_rect: TextureRect = $Hotbar/SelectionRect
+@onready var hotbar_previews_container: Control = $Hotbar/PreviewsContainer
 const BLOCK_PREVIEW_RENDERER := preload("res://src/scenes/block_preview_renderer.tscn")
 const ICON_SCALE := 4.0
 const HOTBAR_SELECTION_TEXTURE_SIZE = 22
@@ -21,8 +22,8 @@ func _ready():
 
 func update_visibility():
 	$OptionsMenu.visible = is_esc_menu_open
-	$UnpausedHUD.visible = not is_esc_menu_open and not f1_hide_hud
-	$FixedHUD.visible = not f1_hide_hud
+	$Crosshair.visible = not is_esc_menu_open and not f1_hide_hud
+	$Hotbar.visible = not f1_hide_hud
 	if is_esc_menu_open:
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	else:
@@ -43,9 +44,9 @@ func _on_quit_button_pressed():
 	SceneController.quit()
 
 func update_selected_block(index: int):
-	var selected_offset = get_element_offset(index)
-	hotbar_selection.offset_left = selected_offset - HOTBAR_SELECTION_TEXTURE_SIZE * ICON_SCALE / 2.0
-	hotbar_selection.offset_right = selected_offset + HOTBAR_SELECTION_TEXTURE_SIZE * ICON_SCALE / 2.0
+	var x_hotbar_offset = get_element_offset(index)
+	hotbar_selection_rect.offset_left = x_hotbar_offset - HOTBAR_SELECTION_TEXTURE_SIZE * ICON_SCALE / 2.0
+	hotbar_selection_rect.offset_right = x_hotbar_offset + HOTBAR_SELECTION_TEXTURE_SIZE * ICON_SCALE / 2.0
 
 func update_hotbar_preview(index: int, face_kind: VoxelWorld.FaceKind):
 	set_hotbar_preview(face_kind, index)
@@ -56,25 +57,24 @@ func get_element_offset(index: int) -> float:
 
 func set_hotbar_preview(face_kind: VoxelWorld.FaceKind, index: int):
 	var rect := TextureRect.new()
-	$FixedHUD/hotbarPreviewcontainer.add_child(rect)
+	hotbar_previews_container.add_child(rect)
 
 	var preview_viewport: SubViewport = BLOCK_PREVIEW_RENDERER.instantiate()
 	rect.add_child(preview_viewport)
 	preview_viewport.setup_block_and_camera(face_kind)
 
-	var preview_offset = get_element_offset(index)
+	var x_hotbar_offset = get_element_offset(index)
 	rect.anchor_left = 0.5
 	rect.anchor_right = 0.5
-	rect.anchor_top = 0.898
-	rect.anchor_bottom = 0.898
 	var preview_size = preview_viewport.size.x
-	rect.offset_left = preview_offset - preview_size / 2.0
-	rect.offset_right = preview_offset + preview_size / 2.0
+	rect.offset_left = x_hotbar_offset - preview_size / 2.0
+	rect.offset_top -= 9
+	rect.offset_right = x_hotbar_offset + preview_size / 2.0
 	call_deferred('__update_preview_rect_texture', rect, preview_viewport)
 
 # Wait for it to render, then get rid of the viewport
 func __update_preview_rect_texture(rect: TextureRect, viewport: SubViewport):
-	# Skip two frames to guarantee that the texture will be available
+	# Skip two frames to guarantee our the texture image will be available
 	await self.get_tree().process_frame
 	await self.get_tree().process_frame
 	rect.texture = ImageTexture.create_from_image(viewport.get_texture().get_image())
